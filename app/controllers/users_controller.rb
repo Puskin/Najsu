677f0f9c964ee38e0c3c8977652bc0ebfabc8ce8
Puskin <mbpuskin@gmail.com>
2012-03-11 23:49:35 +0100

    Footer changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, except: [:new, :create]
   before_filter :correct_user,   only: [:edit, :update, :destroy]
   layout "frontend",             only: [:new]
 
