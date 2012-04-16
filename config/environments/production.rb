718f754888b2429ec6b204b06520510b1aa66cc3
Puskin <mbpuskin@gmail.com>
2012-04-16 21:18:14 +0200

    Trying to solve redirects via rack middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Tuduk::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #Added this for www redirects
+  config.middleware.use "WwwAdd"
+
 end
