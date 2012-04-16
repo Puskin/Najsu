d63f8de54cd30ca22c18988dd7e6c947d2f7dd1a
Puskin <mbpuskin@gmail.com>
2012-04-16 21:37:28 +0200

    Trying to redirect all domains to without www

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Tuduk::Application.initialize!
+
+# Thing to initialize no_www class middleware
+Rails::Initializer.run do |config|
+  config.middleware.use “NoWWW” if RAILS_ENV == ‘production’
+end
