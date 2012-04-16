bee433a5c2a2a7ed770b537cb40ce51d20760c82
Puskin <mbpuskin@gmail.com>
2012-04-16 21:51:18 +0200

    Redirecting domain via before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'www.najsu.pl'
+
+  def ensure_domain
+  	if Rails.env == "production"
+	    if request.env['HTTP_HOST'] != APP_DOMAIN
+	      # HTTP 301 is a "permanent" redirect
+	      redirect_to "http://#{APP_DOMAIN}#{request.env['REQUEST_PATH']}", :status => 301
+	    end
+	   end
+  end
+
 end
