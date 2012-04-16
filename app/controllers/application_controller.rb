75108c3e9f9c8fbc1394d6acc3942237538c09a9
Puskin <mbpuskin@gmail.com>
2012-04-16 20:58:35 +0200

    Removed adding www because its not heroku supported

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
 	
   protect_from_forgery
   include SessionsHelper
-  before_filter :check_uri  	 	
-  
-  def check_uri	
-    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host) if Rails.env == 'production'
-  end                                           
 
 end
