cfc1cd465b5aa9c3a69e6213426b48a7ba8744a3
Puskin <mbpuskin@gmail.com>
2012-04-16 20:34:33 +0200

    Added www redirect again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
 	
   protect_from_forgery
   include SessionsHelper
-
-      
+  before_filter :check_uri  	 	
+  
+  def check_uri	
+    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host) if Rails.env == 'production'
+  end                                           
 
 end
