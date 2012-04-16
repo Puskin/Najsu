9bcaf7d90c908812e615671f451951db9906f4a0
Puskin <mbpuskin@gmail.com>
2012-04-16 20:23:39 +0200

    Remvoed www redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
-  if ENV['RAILS_ENV'] == 'production'        
-  before_filter :check_uri  	 	
-    def check_uri	
-      redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
-    end                                           
-  end         
+      
 
 end
