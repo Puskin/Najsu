ab39de1564b0e74d9282a893124d8e3b7a4053ea
Puskin <mbpuskin@gmail.com>
2012-04-22 19:47:14 +0200

    Trying to avoid items repeating

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,14 +10,13 @@ class PagesController < ApplicationController
       when "newest"
         @movies = Movie.order('movies.created_at DESC').paginate(:page => params[:page])
       when "popular"
-        @movies = Movie.last_week.paginate(:page => params[:page], :order => 'likes_count DESC')
+        @movies = Movie.order('likes_count DESC').paginate(:page => params[:page])
       when "friends"
         @movies = current_user.feed.order('movies.created_at DESC').paginate(:page => params[:page])
       when "discussed"
         @movies = current_user.feed.order('movies.comments_count DESC').paginate(:page => params[:page])
       when "watched"
-        movies = Movie.last_week
-        @movies = movies.watched.paginate(:page => params[:page])
+        @movies = Movie.order('views_count DESC').paginate(:page => params[:page])
       else 
         @movies = current_user.feed.order('movies.created_at DESC').paginate(:page => params[:page])
       end
