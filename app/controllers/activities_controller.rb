f23822ee68946d4866a84236fa8bb29458eb9b64
Puskin <mbpuskin@gmail.com>
2012-05-01 11:37:33 +0200

    Improved sql query for activities to better work on heroku pg

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,10 +4,7 @@ class ActivitiesController < ApplicationController
 
 	def index    
     followed_users = current_user.followed_users.map(&:id)
-    @activities = Activity.where(
-      "user_id = :user_id OR recipient_id = :recipient_id",
-      { :user_id => followed_users, :recipient_id => current_user.id }
-    ).order('created_at DESC')
+    @activities = Activity.find(:all, :conditions => ["user_id in (?) OR recipient_id = ?", followed_users, current_user.id], :order => 'created_at DESC')
   end
 
 
