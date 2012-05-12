2e0eacd35ab035850e145fc64427d6b991acece1
Puskin <mbpuskin@gmail.com>
2012-05-12 19:11:35 +0200

    Various styling fixes

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -8,7 +8,7 @@ class ActivitiesController < ApplicationController
 
 	def index    
     respond_to do |format|
-      format.html { @activities = Activity.feed(current_user).order('created_at DESC').paginate(:page => params[:page]) }
+      format.html { @activities = Activity.feed(current_user).order('created_at DESC') }
       format.js { @activities = Activity.personal(current_user).order('created_at DESC').first(25) }
     end
     @new_activities = current_user.activities_counter
