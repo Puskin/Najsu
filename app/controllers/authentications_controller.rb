70c0e2ed439273b5e25dc85a731d6384ebb6f591
Puskin <mbpuskin@gmail.com>
2012-05-17 20:21:33 +0200

    Closed popup and refreshed main page after facebook login

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,13 +1,15 @@
 # coding: utf-8
 class AuthenticationsController < ApplicationController
+
+  layout "clean"
   
   def create
     omniauth = request.env['omniauth.auth']
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     if authentication
       sign_in authentication.user
-      redirect_to root_path
-      flash.now[:success] = 'Zalogowano pomyślnie, witaj ponownie.'
+      #redirect_to root_path
+      #flash.now[:success] = 'Zalogowano pomyślnie, witaj ponownie.'
     elsif current_user
       current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'], :token => omniauth['credentials']['token'])
       flash.now[:success] = 'Autentykacja udana, możesz teraz logować się przez Facebook.'
@@ -16,8 +18,8 @@ class AuthenticationsController < ApplicationController
       user.apply_omniauth(omniauth)
       user.save!(:validate => false)
       sign_in user
-      redirect_to root_path
-      flash.now[:success] = 'Z powodzeniem zalogowano przez Facebook, witaj w Najsu!'
+      #redirect_to root_path
+      #flash.now[:success] = 'Z powodzeniem zalogowano przez Facebook, witaj w Najsu!'
     end
   end
 
