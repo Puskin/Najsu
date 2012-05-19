0c29284458525786d91abc92c5b3554880adcd9c
Puskin <mbpuskin@gmail.com>
2012-05-19 13:36:25 +0200

    Some user show view improvements, limited activities and showing more if user unseen counter is bigger

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,9 +4,15 @@ class ActivitiesController < ApplicationController
 	before_filter :signed_in_user
 
 	def index    
+		show_count = 50 #number of visible activities
+		unseen_count = current_user.activities_counter
+		if unseen_count > show_count
+			show_count = unseen_count
+		end
+
     respond_to do |format|
-      format.html { @activities = Activity.feed(current_user).order('created_at DESC') }
-      format.js { @activities = Activity.personal(current_user).order('created_at DESC').first(25) }
+      format.html { @activities = Activity.feed(current_user).order('created_at DESC').first(show_count) }
+      format.js { @activities = Activity.personal(current_user).order('created_at DESC').first(show_count) }
     end
     @new_activities = current_user.activities_counter
     current_user.activities_visit_update
