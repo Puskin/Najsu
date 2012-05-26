c50fb3ea1c132752b4df9e02bdd39f4d4b8e47e1
Puskin <mbpuskin@gmail.com>
2012-05-26 16:14:28 +0200

    Turned on invitation log, target on kUba

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -23,8 +23,7 @@ class InvitationsController < ApplicationController
       :name => 'Najsu',
       :description => 'Odkrywaj filmiki dziel ze znajomymi!'
     )
-
-    #invitation = current_user.invitations.create(:uid => @friend_uid)
+    current_user.invitations.create(:uid => @friend_uid)
 
     respond_to do |format| 
       format.js { @status = true }
